2e72d2801c6a51dd552d241b8c95e01d5a8e5121
Tom <tdrichmond@csbsju.edu>
2018-03-13 16:23:11 -0500

    fixed the action listener problem on homepage

--- NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/Homescreen.java
+++ NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/Homescreen.java
@@ -16,14 +16,13 @@ public class Homescreen extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//              public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                      .setAction("Action", null).show();
+//          }
+//        });
     }
 
 }
