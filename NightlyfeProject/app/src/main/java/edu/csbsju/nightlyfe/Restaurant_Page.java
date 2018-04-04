5941785158af6f9856cb2d9c6ce0c260323d9353
GrantSalk <35675287+GrantSalk@users.noreply.github.com>
2018-04-04 14:08:12 -0500

    Fixing running problem

--- NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/Restaurant_Page.java
+++ NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/Restaurant_Page.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Restaurant_Page extends AppCompatActivity {
 
@@ -51,10 +52,20 @@ public class Restaurant_Page extends AppCompatActivity {
     Cursor resultSet = mydatabase.rawQuery("Select * from business where id = '" + key + "'", null);
     String busName = resultSet.getString(1);
     String address = resultSet.getString(3);
-    String phone = resultSet.getString(3);
-    String hours = resultSet.getString(3);
+    String phone = resultSet.getString(6);
+    String hours = resultSet.getString(7);
 
+    TextView BusinessName = findViewById(R.id.BusinessName);
+    BusinessName.setText(busName);
 
+    TextView BusinessAddress = findViewById(R.id.BusinessAddress);
+    BusinessAddress.setText(address);
+
+    TextView BusinessPhone = findViewById(R.id.BusinessPhone);
+    BusinessPhone.setText(phone);
+
+    TextView BusinessHours = findViewById(R.id.BusinessHours);
+    BusinessHours.setText(hours);
 
     }
     //Buttons should be linked to created pages and a "back to search" button should be created
