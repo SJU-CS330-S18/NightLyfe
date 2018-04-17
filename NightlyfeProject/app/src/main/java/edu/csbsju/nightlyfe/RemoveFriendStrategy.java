82cf5a8eac0963d1975395e979f9a4546363677f
tdrichmond <tdrichmond@csbsju.edu>
2018-04-17 11:34:34 -0500

    committing documentation for FriendStrategy, FriendSearch, AddFriendStrategy, RemoveFriendStrategy

--- NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/RemoveFriendStrategy.java
+++ NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/RemoveFriendStrategy.java
@@ -6,18 +6,25 @@ public class RemoveFriendStrategy implements FriendStrategy {
 
     public SQLiteDatabase mydatabase;
 
+    /*
+    Constructor for AddFriendStrategy object
+    @param String friend to add
+    @param String user to associate with friend
+     */
     public RemoveFriendStrategy(String friend, String user){
         //opens database for use
         mydatabase = FriendSearch.mydatabase;
         algorithmInterface(friend,user);
     }
 
+    /*
+    method to add a friend in a AddFriendStrategy object
+    @param String friend to add
+    @param String user to associate with friend
+     */
     public boolean algorithmInterface(String friend, String user){
         mydatabase.execSQL("DELETE FROM friends WHERE user1 = '"+user+"' AND user2 =  '"+friend+"';");
         mydatabase.execSQL("DELETE FROM friends WHERE user1 = '"+friend+"' AND user2 =  '"+user+"';");
         return true;
-        //Intent goToNextActivity = new Intent(getApplicationContext(), FriendsList.class);
-        //goToNextActivity.putExtra("user", user);
-        //startActivity(goToNextActivity);
     }
 }
