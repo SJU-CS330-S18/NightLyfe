fd80b73e4f0721dd765e61c63c085949eaf1fd57
GrantSalk <35675287+GrantSalk@users.noreply.github.com>
2018-04-19 13:46:31 -0500

    Added OwnerBulletinBoard to manifest

--- NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/AdminReview.java
+++ NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/AdminReview.java
@@ -3,11 +3,53 @@ package edu.csbsju.nightlyfe;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 public class AdminReview extends AppCompatActivity {
+
+    public SQLiteDatabase mydatabase;
+    public String user;
+    public String business;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_review);
+
+        //Get user variable from recent page
+        user = getIntent().getStringExtra("user");
+
+        //Opens database used for NightLyfe application
+        mydatabase = openOrCreateDatabase("NightLyfe", MODE_PRIVATE, null);
+
+
+        //Creates a home button to return to the user home screen
+        Button homeBtn = (Button) findViewById(R.id.homeBtn);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToNextActivity = new Intent(getApplicationContext(), Homescreen.class);
+                goToNextActivity.putExtra("user", user);
+                startActivity(goToNextActivity);
+            }
+        });
+
+        //Receives resultSet for all businesses associated with active user--will be location in future
+        Cursor resultSet = mydatabase.rawQuery("Select * from business", null);
+
+        //Gets size of resultset and moves the cursor to the first entry
+        int size = resultSet.getCount();
+        resultSet.moveToFirst();
+
     }
 }
--- NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/RestaurantList.java
+++ NightlyfeProject/app/src/main/java/edu/csbsju/nightlyfe/RestaurantList.java
@@ -13,8 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TableLayout;
-import android.widget.TableLayout.LayoutParams;
+
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
